8d99aeba789e12a2fc920b8ddfba8d95861a36ba
191G0243@rcarbonifera.tecnm.mx <191G0243@rcarbonifera.tecnm.mx>
2022-11-21 16:23:50 -0600

    eliminar

--- ViewModels/ServerViewModel.cs
+++ ViewModels/ServerViewModel.cs
@@ -22,7 +22,6 @@ namespace Comandas_Server.ViewModels
 {
     public class ServerViewModel
     {
-        public int filas = 1;
         private ComandasServices Server;
         public Comanda Com { get; set; }
         public Producto Prod { get; set; }
@@ -31,12 +30,13 @@ namespace Comandas_Server.ViewModels
 
         public ObservableCollection<Producto> ListaProductos { get; set; }
         public ICommand inicio { get; set; }
-
+        public ICommand Cancelar { get; set; }
         public ServerViewModel()
-        {
-            Server = new ComandasServices();
+        {   
+        Server = new ComandasServices();
             Server.ComandaRecibida += Server_ComandaRecibida;
             inicio = new RelayCommand(Iniciar);
+            Cancelar = new RelayCommand<Comanda>(CancelarComanda);
             ListaComandas = new ObservableCollection<Comanda>();
             Com = new Comanda();
            
@@ -55,22 +55,42 @@ namespace Comandas_Server.ViewModels
             Prod.Precio = 40;
             Prod.Cantidad = 5;
             Com.Pedidos.Add(Prod.Nombre,Prod);
+            Prod = new Producto();
+            Prod.Descripcion = "Rica hamburgesa con doble carne";
+            Prod.Nombre = "2";
+            Prod.Tipo = Tipo.platillo;
+            Prod.Precio = 50;
+            Prod.Cantidad = 5;
+            Com.Pedidos.Add(Prod.Nombre, Prod);
+            Prod = new Producto();
+            Prod.Descripcion = "Rica hamburgesa con doble carne";
+            Prod.Nombre = "5";
+            Prod.Tipo = Tipo.platillo;
+            Prod.Precio = 50;
+            Prod.Cantidad = 5;
+            Com.Pedidos.Add(Prod.Nombre, Prod);
             Com.Id = 1231;
             Com.Fecha = DateTime.Now.ToShortTimeString();
             Com.total =100;
             Com.Pedidos[Prod.Nombre].Cantidad+=1;
-            filas += 1;
+         
             ListaComandas.Add(Com);
-
+           
         }
 
+        private void CancelarComanda(Comanda obj)
+        {
+            ListaComandas.Remove(obj);
+        }
 
         private void Server_ComandaRecibida(Comanda obj)
         {
             Application.Current.Dispatcher.Invoke(delegate // <--- HERE
             {
                 ListaComandas.Add(obj);
-                filas += 1;
+               
+                  
+            
 
 
             });
